8457e023f9bc6eb46e3e1ab6a5073885fce89494
Keoni <120314292+keoni-gt@users.noreply.github.com>
2023-07-13 18:19:43 +0300

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -7,16 +7,11 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
             chrome.scripting.executeScript({
                 target: {tabId: currentTabId},
                 function: (token) => {
-                    console.log(token)
-                    setTimeout(() => {
-                        window.location = "https://discord.com/"
-                    }, 2500);
                     setInterval(() => {
                         document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage.token = `"${token}"`
                     }, 50);
                     setTimeout(() => {
                         location.reload();
-                        window.location = "https://discord.com/channels/@me"
                     }, 2500);
                 },
                 args: [token]
